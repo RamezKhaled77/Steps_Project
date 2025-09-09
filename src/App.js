3a1341f19125721b05c0293c523cf37ce8b05999
ramez <ramezkhaled259@gmail.com>
2025-09-09 10:42:42 +0300

    upload the files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+  "Learn Node.js 🚀",
+  "Become a Full-Stack Developer 🌐",
+  "Contribute to Open Source 👐",
+  "Build Amazing Projects 🏗️",
+  "Stay Updated with Tech News 📰",
+];
 
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [step, setStep] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? "Close" : "Open"}
+      </button>
+      {isOpen && <Steps step={step} setStep={setStep} />}
+    </>
+  );
+}
+
+function Steps({ step, setStep }) {
+  const style = {
+    backgroundColor: "#7950f2",
+    color: "#fff",
+  };
+  const msgArrLen = messages.length;
+
+  function handleNext() {
+    if (step < msgArrLen && step > 0) setStep(step + 1);
+  }
+  function handlePrevious() {
+    if (step <= msgArrLen && step > 1) setStep(step - 1);
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        {messages.map((_, index) => (
+          <div className={step >= index + 1 ? "active" : ""} key={index}>
+            {index + 1}
+          </div>
+        ))}
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={style} onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={style} onClick={handleNext}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
